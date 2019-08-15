4f4f661be7b506f2935bbb8c970b9af4b98438b8
Purnendu Kaul <purnendu23@gmail.com>
2019-08-15 00:35:15 -0500

    fixed all feature engg functions, added loop for lags

--- feature_engg_utils.py
+++ feature_engg_utils.py
@@ -2,13 +2,15 @@ from utils import nearest_smaller
 import numpy as np
 
 
+
 def lagged_shop_item_sales(train_test_set, lag):    
     feature_name = "shop_item_sales_lag" + str(lag)
     train_test_set_c = train_test_set[['shop_id', 'item_id', 'date_block_num', 'item_cnt_month']]
     train_test_set_c.date_block_num = train_test_set_c.date_block_num + lag
     train_test_set_c.rename(columns={'item_cnt_month': feature_name}, inplace=True)
     train_test_set = train_test_set.merge(train_test_set_c, on=['item_id', 'shop_id', 'date_block_num'], how='left').fillna(0)
-    return train_test_set, feature_name
+    return train_test_set
+
 
 
 def lagged_item_sales(train_test_set, lag):    
@@ -17,7 +19,8 @@ def lagged_item_sales(train_test_set, lag):
     train_test_set_c.rename(columns={'item_cnt_month': feature_name}, inplace=True)
     train_test_set_c.date_block_num = train_test_set_c.date_block_num + lag
     train_test_set = train_test_set.merge(train_test_set_c, on=['item_id', 'date_block_num'], how='left').fillna(0)
-    return train_test_set, feature_name
+    return train_test_set
+
 
 
 def lagged_shop_item_price(train_test_set, lag):
@@ -26,7 +29,8 @@ def lagged_shop_item_price(train_test_set, lag):
     train_test_set_c.rename(columns={'avg_item_price': feature_name}, inplace=True)
     train_test_set_c.date_block_num = train_test_set_c.date_block_num + lag
     train_test_set = train_test_set.merge(train_test_set_c, on=['shop_id', 'item_id', 'date_block_num'], how='left').fillna(0)
-    return train_test_set, feature_name
+    return train_test_set
+
 
 
 def lagged_item_price(train_test_set, lag):
@@ -35,34 +39,7 @@ def lagged_item_price(train_test_set, lag):
     train_test_set_c.rename(columns={'avg_item_price': feature_name}, inplace=True)
     train_test_set_c.date_block_num = train_test_set_c.date_block_num + lag
     train_test_set = train_test_set.merge(train_test_set_c, on=['item_id', 'date_block_num'], how='left').fillna(0)
-    return train_test_set, feature_name
-    
-
-# def months_from_lastsale_item(data):
-#     tmp = data[data.item_cnt_month>0].groupby(['item_id']).agg(list_sale_months = ('date_block_num', set)).reset_index()
-#     tmp['list_sale_months'] = tmp['list_sale_months'].apply(list).apply(np.sort)
-#     tmp = data[['date_block_num', 'item_id']].merge(tmp, on=['item_id'], how='left')
-#     tmp.drop_duplicates(['date_block_num', 'item_id'], keep='first', inplace=True)
-#     tmp['lastsale_month'] = \
-#     tmp.apply(lambda row: nearest_smaller(row['date_block_num'], row['list_sale_months']) if row['list_sale_months'] is not np.NaN  else -1, axis=1 )
-#     tmp['months_from_lastsale_item'] = tmp.apply(lambda r: r['date_block_num']-r['lastsale_month'] if r['lastsale_month'] > -1 else -1, axis=1 )
-#     tmp.drop(columns=['lastsale_month', 'list_sale_months'], inplace = True)
-#     data = data.merge(tmp, on=['date_block_num','item_id'], how='left')
-#     return data
-
-
-# def months_from_lastsale_shop_item(data):    
-#     tmp = data[data.item_cnt_month>0].groupby(['shop_id', 'item_id']).agg(list_sale_months = ('date_block_num', list)).reset_index()
-#     data = data.merge(tmp, on=['shop_id', 'item_id'], how='left')
-#     data['lastsale_month_shop_item'] = \
-#     data.apply(lambda row: nearest_smaller(row['date_block_num'], row['list_sale_months']) if row['list_sale_months'] is not np.NaN  else -1, axis=1 )
-
-#     data['months_from_lastsale_shop_item'] = \
-#     data.apply(lambda r: r['date_block_num']-r['lastsale_month_shop_item'] if r['lastsale_month_shop_item'] > -1 else -1, axis=1 )
-
-#     data.drop(columns=['list_sale_months', 'lastsale_month_shop_item'], inplace= True)
-#     return data
-
+    return train_test_set
 
 
 def months_from_last_shopitem_sale(data, lags):
@@ -76,7 +53,7 @@ def months_from_last_shopitem_sale(data, lags):
         data_c[feature_name] = 1
         data = data.merge(data_c, on=['item_id', 'date_block_num', 'shop_id'], how='left').fillna(0)
         data_c.drop(columns = feature_name, inplace=True)
-    return data, feat_names
+    return data
 
 
 
@@ -91,4 +68,4 @@ def months_from_last_item_sale(data, lags):
         data_c[feature_name] = 1
         data = data.merge(data_c, on=['item_id', 'date_block_num'], how='left').fillna(0)
         data_c.drop(columns = feature_name, inplace=True)
-    return data, feat_names
+    return data
